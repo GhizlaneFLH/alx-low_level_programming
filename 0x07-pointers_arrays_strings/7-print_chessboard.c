d780b7e6ccb82cd90cf41fef4ffe51c1a87ae450
GhizlaneFLH <fellahghizlan02@gmail.com>
2023-07-07 18:01:25 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int e;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (e = 0; e < 8; e++)
+			_putchar(a[i][e]);
+		_putchar('\n');
+	}
+}
